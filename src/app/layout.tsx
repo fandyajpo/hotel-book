6176a54bd2f0260b49810f669b501bf0503b20aa
Fandoy <fandyglitch3@gmail.com>
2024-01-04 18:44:17 +0800

    xsawda

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Contact from "@/components/Backoffice/Contact";
 import ReactQuery from "@/provider/TanstackQuery";
 import Footer from "@/components/Layout/Footer";
 import Announce from "@/components/Layout/Announce";
+import Script from "next/script";
 const inter = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -34,7 +35,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-KFZRQQHC');`}
+        </Script>
+      </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KFZRQQHC"
+            height="0"
+            width="0"
+            className="hidden"
+          ></iframe>
+        </noscript>
         <ReactQuery>
           <Announce />
           <Contact />
